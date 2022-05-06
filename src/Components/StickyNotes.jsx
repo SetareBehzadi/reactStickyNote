9c891622fa08b176f336e38b3c0fa39144fcc1f1
SetareBehzadi <setare.behzadi@gmail.com>
2022-05-06 20:54:52 +0430

    sticky note design

---
+++ src/Components/StickyNotes.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Note from './Note'
+
+export default function StickyNotes(){
+    const notesData = [
+        { title:'Title #1' , content:'Text Content #1 '},
+        { title:'Title #2' , content:'Text Content #2 '},
+        { title:'Title #3' , content:'Text Content #3 '},
+        { title:'Title #4' , content:'Text Content #4 '},
+        { title:'Title #5' , content:'Text Content #5 '},
+        { title:'Title #6' , content:'Text Content #6 '},
+        { title:'Title #7' , content:'Text Content #7 '},
+    ];
+
+   const notes = notesData.map((note , index) =>  <Note key={index} title ={note.title} content={note.content} /> );
+    return <div>
+        <ul>
+            {notes}
+        </ul>
+    </div>
+}
